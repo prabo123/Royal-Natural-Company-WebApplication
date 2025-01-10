115c7cb456a5096e5b7808d5c44ab419e86bd62d
Prabodhani Gamage <165150283+prabo123@users.noreply.github.com>
2025-01-11 00:57:27 +0530

    backend

---
+++ Desktop/royal/Backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+//middle ware
+app.use(express.json());
+app.use(cors());
+
+//listen to a port
+app.listen ( 5000,()=>{
+console.log("SEVER STARTED")
+})
